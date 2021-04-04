cede4a88825b7c036f25727e33a14e5584f82811
Marco Regueira <marcoregueira@gmail.com>
2021-04-04 23:44:01 +0200

    Refactor Central Configuration
    - Now, unused values will be saved as NULL, those values will be removed and not be sent to agents.
    - Better code in controller

--- src/H2h.RubberBand.Server/H2h.RubberBand.Server/Controllers/ConfigController.cs
+++ src/H2h.RubberBand.Server/H2h.RubberBand.Server/Controllers/ConfigController.cs
@@ -4,6 +4,7 @@ using H2h.RubberBand.Server.Options;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace H2h.RubberBand.Server.Controllers
@@ -11,11 +12,11 @@ namespace H2h.RubberBand.Server.Controllers
     public class ConfigController : Controller
     {
         private readonly ApmOptions apmOptions;
-        private readonly IConfigCrud crud;
+        private readonly IConfigCrud configRepository;
 
         public ConfigController(IConfigCrud crud, IOptions<ApmOptions> apmOptions)
         {
-            this.crud = crud;
+            this.configRepository = crud;
             this.apmOptions = apmOptions.Value;
         }
 
@@ -24,47 +25,59 @@ namespace H2h.RubberBand.Server.Controllers
         [Route("/config/v1/agents")]
         public async Task<IActionResult> ConfigAsync(
             [FromQuery(Name = "service.name")] string serviceName,
-            [FromQuery(Name = "service.environment")] string serviceEnvironment
-            )
+            [FromQuery(Name = "service.environment")] string serviceEnvironment)
+
         {
             if (string.IsNullOrWhiteSpace(serviceName) || string.IsNullOrWhiteSpace(serviceEnvironment))
                 return BadRequest();
 
-            var options = await crud.GetConfigAsync(serviceName, serviceEnvironment);
-            if (options.config != null)
+            (var responseExpiration, var serviceConfig) = await configRepository.GetConfigAsync(serviceName, serviceEnvironment);
+
+            if (serviceConfig == null)
             {
-                Response.Headers.Remove("Cache-Control");
-                Response.Headers.Add("Cache-Control", $"public,max-age={options.maxAgeSeconds}");
-                return Ok(options.config);
+                serviceConfig = await GenerateNewConfiguration(serviceName, serviceEnvironment, serviceConfig);
+                responseExpiration = apmOptions.DefaultCentralConfigurationMaxAge;
             }
 
-            Dictionary<string, string> resultTemplate = GetOptionsTemplate();
-            await SaveDefaultOptionsIfRequired(serviceName, serviceEnvironment, options, resultTemplate);
+            RemoveNullEntries(serviceConfig);
 
             Response.Headers.Remove("Cache-Control");
-            Response.Headers.Add("Cache-Control", $"public,max-age={apmOptions.DefaultCentralConfigurationMaxAge}");
-
-            return Ok(resultTemplate);
+            Response.Headers.Add("Cache-Control", $"public,max-age={responseExpiration}");
+            return Ok(serviceConfig);
         }
 
-        private async Task SaveDefaultOptionsIfRequired(string serviceName, string serviceEnvironment, (int? maxAgeSeconds, Dictionary<string, string> config) options, Dictionary<string, string> result)
+        private static void RemoveNullEntries(Dictionary<string, string> config)
         {
-            if (options.config == null && apmOptions.AutoCreateCentralConfiguration)
-                await crud.SaveConfig(serviceName, serviceEnvironment, apmOptions.DefaultCentralConfigurationMaxAge, result);
+            foreach (var pair in config.ToList().Where(x => x.Value == null))
+                config.Remove(pair.Key);
+        }
+
+        private async Task<Dictionary<string, string>> GenerateNewConfiguration(string serviceName, string serviceEnvironment, Dictionary<string, string> config)
+        {
+            config = GetOptionsTemplate();
+            if (apmOptions.AutoCreateCentralConfiguration)
+                await configRepository.SaveConfig(serviceName, serviceEnvironment, apmOptions.DefaultCentralConfigurationMaxAge, config);
+            return config;
         }
 
         private static Dictionary<string, string> GetOptionsTemplate()
         {
             return new Dictionary<string, string>()
                 {
-                    {"_capture_body"                             ,""},
-                    {"_capture_body_content_types"               ,""},
-                    {"_transaction_max_spans"                    ,""},
-                    {"_transaction_sample_rate"                  ,""},
-                    {"_capture_headers"                          ,""},
-                    {"_log_level"                                ,""},
-                    {"_span_frames_min_duration"                 ,""},
-                    {"_stack_trace_limit"                        ,""}
+                    {"capture_body"                             ,null},
+                    {"capture_body_content_types"               ,null},
+                    {"capture_headers"                          ,null},
+
+                    {"log_level"                                ,null},
+                    {"recording"                                ,null},
+
+                    {"sanitize_field_names"                     ,null},
+                    {"span_frames_min_duration"                 ,null},
+                    {"stack_trace_limit"                        ,null},
+
+                    {"transaction_ignore_urls"                  ,null},
+                    {"transaction_max_spans"                    ,null},
+                    {"transaction_sample_rate"                  ,null},
                 };
         }
     }
--- src/H2h.RubberBand.Server/H2h.RubberBand.Server/Options/ApmOptions.cs
+++ src/H2h.RubberBand.Server/H2h.RubberBand.Server/Options/ApmOptions.cs
@@ -2,6 +2,9 @@
 {
     public class ApmOptions
     {
+        /// <summary>
+        /// Time in seconds
+        /// </summary>
         public int DefaultCentralConfigurationMaxAge { get; set; } = 300;
         public bool AutoCreateCentralConfiguration { get; set; }
         public bool RumEnabled { get; set; }
